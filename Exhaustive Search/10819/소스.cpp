f9c2929ea9f13191141013dea8edf6e716222ccf
choihj105 <li0_0li2@naver.com>
2021-12-21 22:52:01 +0900

    refactor : retry 'recursive style'

--- Exhaustive Search/10819/소스.cpp
+++ Exhaustive Search/10819/소스.cpp
@@ -7,54 +7,133 @@
 */
 
 // 1. 전략: 완전탐색 .. n값은 3 <= N <= 8 조건이기에 무작정 찾는 모든 후보검색을 쓰기로 함
-// 모든 후보검색 O(2**n)
 // DFS 깊이우선탐색, Back Tracking .. 
 
+
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int arr[10];
-int check[10];
-int use_arr[10];
-int n;
-int max_n = 0;
+int A[10]; // 주어진 배열
+int n; // 정수 개수
+int basket[10]; // 차례대로 배열을 담을 통
+bool basket_check[10]; // 통에 배열이 잠겨있는지 확인하는 용도
+int MAX_result = 0; // 최대 값
 
 int cal() {
 	int result = 0;
-
-	for (int i = 0; i < n-1; i++) {
-		result += abs(use_arr[i] - use_arr[i + 1]);
+	for (int i = 0; i < n-1, i++) { 
+		result += abs(basket[i] - basket[i + 1]);
 	}
 	return result;
 }
 
-void dfs(int cnt) {
-	if (cnt == n) { max_n = max(max_n, cal()); return; }
+// 1. 재귀적 방법
+void DFS(int cnt) { 
+	if (cnt == n) {
+		MAX_result = max(MAX_result, cal()); return;
+	}
 
 	for (int i = 0; i < n; i++) {
-		if (check[i]) continue;
 
-		use_arr[cnt] = arr[i];
-		check[i] = 1;
-		dfs(cnt + 1);
-		check[i] = 0;
+		if (basket_check[i]) continue;
+
+		basket[cnt] = A[i];
+		basket_check[i] = true;
+		DFS(cnt + 1);
+		basket_check[i] = false;
 	}
 }
+// 겹치지않은 순열 순서대로 basket에 집어넣어보면서 값을 나오게 하는 알고리즘이다.
 
-int main() 
-{
+int main() {
+
 	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-
+	
 	for (int i = 0; i < n; i++) {
-		check[i] = 1;
-		use_arr[0] = arr[i];
-		dfs(1);
-		check[i] = 0;
+		cin >> A[i];
 	}
 
-	cout << max_n;
-
+	cout << MAX_result;
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int arr[10];
+//int check[10];
+//int use_arr[10];
+//int n;
+//int max_n = 0;
+//
+//// 완성된 배열의 값을 알기위한 계산함수
+//int cal() {
+//	int result = 0;
+//
+//	for (int i = 0; i < n-1; i++) {
+//		result += abs(use_arr[i] - use_arr[i + 1]);
+//	}
+//	return result;
+//}
+//
+//// 1. 재귀 함수로 구현하기
+//void dfs(int cnt) {
+//	if (cnt == n) { max_n = max(max_n, cal()); return; }
+//
+//	for (int i = 0; i < n; i++) {
+//		if (check[i]) continue;
+//
+//		use_arr[cnt] = arr[i];
+//		check[i] = 1;
+//		dfs(cnt + 1);
+//		check[i] = 0;
+//	}
+//}
+//
+//int main() 
+//{
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//		cin >> arr[i];
+//
+//	for (int i = 0; i < n; i++) {
+//		check[i] = 1;
+//		use_arr[0] = arr[i];
+//		dfs(1);
+//		check[i] = 0;
+//	}
+//
+//	cout << max_n;
+//
+//}
