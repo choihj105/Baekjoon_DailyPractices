591f33ec1f9850189526a8ac33543593fec38aeb
choihj105 <li0_0li2@naver.com>
2022-01-06 13:46:29 +0900

    feat : complete '4375'

--- 4375/4375.cpp
+++ 4375/4375.cpp
@@ -6,81 +6,37 @@
 
 * Solution
 1) 문제 스타일 그대로 푸는 경우 -> 시간초과(1초)
-2)							 
+2) 
 
 */
 
 #include <iostream>
 using namespace std;
 
-	
 int main()
 {
 	int n; // 입력 n
-	long long m; // n의 배수값
-	int cnt; // 결과값
+	long long m; // 1로 이루어진 값 -> 1, 11, 111 ...
+	int cnt; // 결과값, 자릿수
+	
 	
 	while (cin >> n) {
-		m = 1; cnt = 0;
+		if (cin.eof()) break;
+		
+		m = 1; cnt = 1;
 
 		while (true) {
+			
 			if (m % n == 0) {
-				cnt += 1;
 				cout << cnt << endl;
 				break;
 			}
-			m = m * 10 + 1;
-		}
-	
-	}
-}
-
-
-/*
-int cal(int n) {
-
-	// 배수 m, 자리수 result
-
-	int m = 1, result = 0;
-
-	while (true)
-	{
-		// 배수 m 를 찾아 냈을 때
-		if (m % n == 0)
-		{
-			do {
-				result += 1;
-			} while (m /= 10);
-			return result;
-
-		}
-		// m을 -> 1 -> 11 -> 111 -> ...
-		else {
-			m = m * 10 + 1;
+			else {
+				//m %= n; // m의 수를 미리 mod 해줘서 속도를 높힌다.
+				m = m * 10 + 1;
+				cnt += 1;
+			}
+		
 		}
 	}
-
 }
-
-int main()
-{
-	// 주어진 수 n, 배수 m, 자리수 k
-	int n[3];
-	int k[3];
-
-	// 입력문
-	for (int i = 0; i < 3; i++) {
-
-		cin >> n[i];
-	}
-
-	// 출력문
-	for (int i = 0; i < 3; i++) {
-
-		k[i] = cal(n[i]);
-		cout << k[i] << endl;
-	}
-
-}
-
-*/
