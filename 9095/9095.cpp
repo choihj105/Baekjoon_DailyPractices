e22d5c5c73d9687493db50b2ec329fd68ae74a78
choihj105 <li0_0li2@naver.com>
2022-01-14 02:05:09 +0900

    feat : complete '9095'

--- 9095/9095.cpp
+++ 9095/9095.cpp
@@ -17,3 +17,28 @@ sol)
 3/ 1+1+1/ 1+2/ 2+1/ 3
 
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t = 0 , tmp = 0;
+	cin >> t;
+
+	int *input = new int[t];
+	
+	for (int k = 0; k < t; k++) {
+		cin >> input[k];
+	}
+
+
+	int n[11] = { 0, 1, 2, 4, 0, }; // idx == number n
+
+	for (int i = 4; i < 11; i++) {
+		n[i] = n[i - 1] + n[i - 2] + n[i - 3];
+	}
+
+	for (int j = 0; j < t; j++) {
+		cout << n[(input[j])] << endl;
+	}
+}
