88310208a496d65553eb971d57a11b23acc0d03f
choihj105 <li0_0li2@naver.com>
2022-01-14 08:18:39 +0900

    feat : complete '1718'

--- 1748/1748.cpp
+++ 1748/1748.cpp
@@ -18,18 +18,33 @@ ex) int n[3]   // n[0]은 x  n[1]부터 시작 // n[1]은 = 9, n[2] = 90 *
 */
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
 	long long int n; // input
-	long long int output; // output
+	long long int output = 0; // output
+	
 	int k =0; // n의 자릿수
-
+	int bucket[10] = { 0, }; // 0은 제외한 1부터 9번째까지의 자릿수 합 ex) n[1] = 9*1 ,n[2] = 90*2
+	
+	for (int i = 1; i < 10; i++) {
+		bucket[i] = 9 * i * (long long int)pow(10, i - 1);
+	}
+	
 	cin >> n; // 1<= n <= 100,000,000
 
 	for (int i = n; i != 0; i /= 10) {
 		k++;
 	}
+
+	for (int i = 1; i < k; i++) {
+		output += bucket[i];
+	}
+
+	output += (n - pow(10, k - 1) + 1) * k;
+	
+	cout << output;
 
 	
 
