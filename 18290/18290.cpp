f63d26252634ba791e89220b575ded85132a943b
choihj105 <li0_0li2@naver.com>
2022-01-30 18:26:36 +0900

    feat : complete '18290'

--- 18290/18290.cpp
+++ 18290/18290.cpp
@@ -21,55 +21,53 @@ int arr[100] = {0, };
 
 int grid[MAX][MAX];
 bool visited[MAX][MAX] = { 0, };
-bool adjacent[MAX][MAX] = { 0, };
+int adjacent[MAX][MAX] = { 0, };
 
 int n, m, k;
-int result = 0;
+int result = -1000000;
 
 
 void switchAdjacent(int n_ , int m_, bool toggle) {
+	int k;
+	if (toggle == true) k = 1;
+	if (toggle == false) k = -1;
 
-	if (toggle) {
-		if (n_ > 0) adjacent[n_ - 1][m_] = true;
-		if (n_ < n-1) adjacent[n_ + 1][m_] = true;
-		if (m_ > 0) adjacent[n_][m_ - 1] = true;
-		if (m_ < m- 1) adjacent[n_][m_ + 1] = true;
-		
-	}
-	else {
-		if (n_ > 0) adjacent[n_ - 1][m_] = false;
-		if (n_ < n - 1) adjacent[n_ + 1][m_] = false;
-		if (m_ > 0) adjacent[n_][m_ - 1] = false;
-		if (m_ < m - 1) adjacent[n_][m_ + 1] = false;
-
-	}
-	
+	if (n_ > 0) adjacent[n_ - 1][m_] += k;
+	if (n_ < n - 1) adjacent[n_ + 1][m_] += k;
+	if (m_ > 0) adjacent[n_][m_ - 1] += k;
+	if (m_ < m - 1) adjacent[n_][m_ + 1] += k;
 }
 
 
 
 void dfs(int cnt = 0) {
-	int tmp = 0;
 
 	if (cnt == k) {
+		int tmp = 0;
+		for (int i = 0; i < k; i++) { 
+			tmp += arr[i]; 
+		
+		}
+		
 
-		for (int i = 0; i < k; i++) tmp += arr[i];
 		if (tmp > result) { result = tmp; }
+		
 		return;
 	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 
-			if (!visited[i][j] && (!adjacent[i][j]))
-				visited[i][j] = true;
-				
-				switchAdjacent(i, j, true);
-				arr[cnt] = grid[i][j];
-				dfs(cnt + 1);
-				
-				visited[i][j] = false;
-				switchAdjacent(i, j, false);
+			if (!visited[i][j] && !adjacent[i][j]) {
+			visited[i][j] = true;
+
+			switchAdjacent(i, j, true);
+			arr[cnt] = grid[i][j];
+			dfs(cnt + 1);
+
+			visited[i][j] = false;
+			switchAdjacent(i, j, false);
+			}
 		}
 	}
 
@@ -91,11 +89,5 @@ int main() {
 	dfs();
 	
 	cout << result;
-	
-
-
-	
-
-
 	
 }
