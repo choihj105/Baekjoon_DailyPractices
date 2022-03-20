318df678667f5f19ad042c01a3305733bd4c0b3b
choihj105 <li0_0li2@naver.com>
2022-03-20 20:26:33 +0900

    feat : complete '1152'

--- 1_weeks_chobo/1152/1152.cpp
+++ 1_weeks_chobo/1152/1152.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	
+	string A;
+	int cnt = 0;
+	getline(cin, A);
 
+
+	for (int i = 0; i < A.length(); i++) {
+		if (A[i] == ' ') ++cnt;
+	}
+
+	if (A.front() == ' ') --cnt;
+	if (A.back() != ' ') ++cnt;
+	
+	cout << cnt;
 }
