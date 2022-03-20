6d62e20bf840914fc8c02ada496535073ab184df
choihj105 <li0_0li2@naver.com>
2022-03-20 20:32:44 +0900

    feat : complete Short code '1152'

--- 1_weeks_chobo/1152/1152.cpp
+++ 1_weeks_chobo/1152/1152.cpp
@@ -3,18 +3,26 @@
 using namespace std;
 
 int main() {
-	
-	string A;
+
 	int cnt = 0;
-	getline(cin, A);
-
-
-	for (int i = 0; i < A.length(); i++) {
-		if (A[i] == ' ') ++cnt;
+	string str;
+	
+	while (cin >> str) {
+		++cnt;
 	}
 
-	if (A.front() == ' ') --cnt;
-	if (A.back() != ' ') ++cnt;
-	
 	cout << cnt;
 }
+
+//string A;
+//int cnt = 0;
+//getline(cin, A);
+
+
+//for (int i = 0; i < A.length(); i++) {
+//	if (A[i] == ' ') ++cnt;
+//}
+
+//if (A.front() == ' ') --cnt;
+//if (A.back() != ' ') ++cnt;
+//
