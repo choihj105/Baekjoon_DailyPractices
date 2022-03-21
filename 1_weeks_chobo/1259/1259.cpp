13700b9b8864db4af5d2302f9ae67092068f5f22
choihj105 <li0_0li2@naver.com>
2022-03-21 15:08:50 +0900

    refactor : short coding '1259' using reverse algorithm

--- 1_weeks_chobo/1259/1259.cpp
+++ 1_weeks_chobo/1259/1259.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
+#include<algorithm>
 #include <string>
 using namespace std;
 
 int main() {
-
-	string str;
 	
-	while (true) {
-		cin >> str;
-		if (str == "0") break;
-
-		string tmp = str;
-		
-		for (int i = 0; i < (str.length())/2; i++) {
-			swap(str[i], str[str.length() -i-1]);
-		}
-		
-		if (str == tmp) cout << "yes" << "\n";
-		else cout << "no" << "\n";
-
+	string s;
+	while (cin >> s) {
+		if (s == "0") break;
+		string rs = s;
+		reverse(rs.begin(), rs.end());
+		cout << ((s == rs) ? "yes" : "no") << endl;
 	}
 
-	
 }
