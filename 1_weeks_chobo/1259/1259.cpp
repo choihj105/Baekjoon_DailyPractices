b894d9f838c485aa3630a666bef45199449cff64
choihj105 <li0_0li2@naver.com>
2022-03-21 14:59:38 +0900

    feat: complete '1259'

--- 1_weeks_chobo/1259/1259.cpp
+++ 1_weeks_chobo/1259/1259.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
 
+	string str;
+	
+	while (true) {
+		cin >> str;
+		if (str == "0") break;
+
+		string tmp = str;
+		
+		for (int i = 0; i < (str.length())/2; i++) {
+			swap(str[i], str[str.length() -i-1]);
+		}
+		
+		if (str == tmp) cout << "yes" << "\n";
+		else cout << "no" << "\n";
+
+	}
+
+	
 }
