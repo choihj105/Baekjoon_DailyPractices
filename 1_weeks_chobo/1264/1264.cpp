19e72eb981947ffc75beb729b39bfb49499954e2
choihj105 <li0_0li2@naver.com>
2022-03-21 15:19:43 +0900

    feat: complete '1264'

--- 1_weeks_chobo/1264/1264.cpp
+++ 1_weeks_chobo/1264/1264.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	string str;
+	
+	
+	while (getline(cin,str)) {
+		if (str == "#") break;
+		int cnt = 0;
+		
+		for (auto it = str.begin(); it != str.end(); it++) {
+			if (*it == 'a' || *it == 'A') cnt++;
+			if (*it == 'e' || *it == 'E') cnt++;
+			if (*it == 'i' || *it == 'I') cnt++;
+			if (*it == 'o' || *it == 'O') cnt++;
+			if (*it == 'u' || *it == 'U') cnt++;
+		}
+		
+		cout << cnt << endl;
+		
+	}
 
 }
