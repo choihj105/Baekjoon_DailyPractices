bfc6ae7d631e8f719737679abe5699600769d4cb
choihj105 <li0_0li2@naver.com>
2022-03-22 21:07:20 +0900

    feat : complete '1568'

--- 2_weeks_chobo/1568/1568.cpp
+++ 2_weeks_chobo/1568/1568.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+	int N;
+	cin >> N;
+
+	int time = 0;
+	for (int i = 1; N > 0; i++) {
+		
+		if (N < i) i = 1;
+		time++;
+		N -= i;
+	}
+
+	cout << time;
+
 
 }
+
