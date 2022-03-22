bed2002cb714a93b7ef8badf870c0ca30c53b6c3
choihj105 <li0_0li2@naver.com>
2022-03-22 20:43:58 +0900

    feat : complete '1547'

--- 2_weeks_chobo/1547/1547.cpp
+++ 2_weeks_chobo/1547/1547.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+	bool cup[3] = {true, false,};
+	int M;
+	cin >> M;
+	while (M--) {
+		int A, B;
+		cin >> A >> B;
+		A--, B--;
+		if (cup[A]) {
+			cup[A] = false, cup[B] = true;
+			continue;
+		}
+		if (cup[B]) {
+			cup[B] = false, cup[A] = true;
+			continue;
+		}
+	
+	}
+
+	for (int i = 0; i < 3; i++) {
+		if (cup[i]) cout << ++i;
+	}
 
 }
