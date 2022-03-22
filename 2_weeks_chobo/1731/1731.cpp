688ad7e72b2d1d13da5b367581a8fc0523f11117
choihj105 <li0_0li2@naver.com>
2022-03-22 21:23:21 +0900

    feat : complete '1731'

--- 2_weeks_chobo/1731/1731.cpp
+++ 2_weeks_chobo/1731/1731.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+	int N;
+	cin >> N;
 
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	int tmp = c;
+	while (N-- -3) { cin >> tmp;}
+
+	tmp = ((b - a) == (c - b)) ? tmp + (b - a) : tmp * (b / a);
+
+	cout << tmp;
 }
