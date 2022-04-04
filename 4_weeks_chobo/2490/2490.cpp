52f3ae2e326ca44432714aaedcc04f481cafc020
choihj105 <li0_0li2@naver.com>
2022-04-05 08:46:12 +0900

    feat : Complete '2490'

--- 4_weeks_chobo/2490/2490.cpp
+++ 4_weeks_chobo/2490/2490.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main() 
 {
+	char yut[5] = { 'E', 'A', 'B', 'C', 'D' };
+	
+	int n = 3;
+	while (n--) {
+		bool deung; int tmp = 0;
+		for (int i = 0; i < 4; i++) {
+			cin >> deung;
+			if (!deung) tmp++;
+		}
 
+		cout << yut[tmp] << '\n';
+	}
 }
