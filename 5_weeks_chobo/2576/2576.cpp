f3735b705a497f0ca55018b626ec108db3e587f9
choihj105 <li0_0li2@naver.com>
2022-04-11 09:17:30 +0900

    feat : Complete '2576'

--- 5_weeks_chobo/2576/2576.cpp
+++ 5_weeks_chobo/2576/2576.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
+	
+	int total = 0, min = 100;
+	for (int i = 0; i < 7; i++) {
+		int tmp; cin >> tmp;
+		
+		if (tmp % 2 != 0) {
+			total += tmp;
+			min = min > tmp ? tmp : min;
+		}
+	}
 
-
+	if (total != 0) {
+		cout << total << endl;
+		cout << min << endl;
+	}
+	else {
+		cout << -1 << endl;
+	}
+	
 }
