1da0fc380244e41daaefce0bc91575256fa17f0a
choihj105 <li0_0li2@naver.com>
2022-04-11 09:03:03 +0900

    feat : Complete '2562'

--- 5_weeks_chobo/2562/2562.cpp
+++ 5_weeks_chobo/2562/2562.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main() {
 
+	int idx;
+	int max = 0;
 
+	for (int i = 0; i < 9; i++) {
+		int tmp;
+		cin >> tmp;
+
+		if (max < tmp) {
+			max = tmp;
+			idx = i + 1; // 0번째가 아닌 1번째부터 시작
+		}
+	}
+	cout << max << endl;
+	cout << idx << endl;
 }
