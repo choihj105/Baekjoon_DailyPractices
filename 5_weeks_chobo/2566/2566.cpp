02a70e7131064b02d8a9bf4f9a5e9ce6b0bd39f8
choihj105 <li0_0li2@naver.com>
2022-04-11 09:07:57 +0900

    feat : Complete '2566'

--- 5_weeks_chobo/2566/2566.cpp
+++ 5_weeks_chobo/2566/2566.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main() {
 
+	int cIdx, rIdx;
+	int max = 0 ;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			int tmp; cin >> tmp;
+
+			if (max < tmp) {
+				max = tmp;
+				cIdx = i + 1;
+				rIdx = j + 1;
+			}
+		}
+	}
+	
+	cout << max << endl;
+	cout << cIdx<< ' ' << rIdx <<endl;
+
 
 }
