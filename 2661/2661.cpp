22ead25cffb8a5f9dfd56a2bc692154d54eeeb3f
choihj105 <li0_0li2@naver.com>
2022-04-12 18:16:13 +0900

    feat : Complete '2661' using DFS, backtraking

---
+++ 2661/2661.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+int arr[80];
+int n;
+
+bool isSame(int diff, int end) {
+
+	//for (int i = 0; i + 2*diff<= end; i++) {
+	//	
+	//	
+	//	int isFlag = 0; // 서로 다르다
+	//	for (int j = 0; j < diff; j++)
+	//		if (arr[i+j] == arr[i + diff +j]) isFlag += 1;
+
+	//	if (isFlag) return true;
+	//}
+
+	//return false;
+	string a;
+	for (int i = 0; i < end; i++) {
+		a.push_back(arr[i]);
+	}
+
+	for (int i = 0; i + diff * 2 <= end; i++) {
+		if (a.substr(i, diff) == a.substr(i + diff, diff)) return true;
+	}
+
+	return false;
+}
+
+bool check(int cnt) {
+	
+	int end = cnt + 1;
+
+	// 1 ~ cnt/2 숫자 확인
+	for (int i = 1; i <= end/ 2; i++) {
+		
+		if (isSame(i, end)) return false;
+	}
+	return true;
+}
+
+
+void DFS(int cnt) {
+	
+	if (cnt == n) {
+		for (int i = 0; i < n; i++) {
+			cout << arr[i];
+		}
+		exit(0);
+	}
+
+	for (int i = 1; i < 4; i++) {
+
+		// 1, 2, 3 넣기
+		arr[cnt] = i;
+
+		// check() 함수
+		if (check(cnt)) DFS(cnt + 1); 
+	}
+
+
+}
+
+
+int main() {
+
+	cin >> n;
+	DFS(0);
+
+}
