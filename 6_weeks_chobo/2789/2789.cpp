ec178ca5b0bb297c6f166bde7a7bfbec1dc25e91
choihj105 <li0_0li2@naver.com>
2022-04-19 10:57:35 +0900

    feat : Complete '2789' using sort, compare

--- 6_weeks_chobo/2789/2789.cpp
+++ 6_weeks_chobo/2789/2789.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
+bool compare(const pair<int, int>& a, const pair<int, int>& b) {
+	return a.second < b.second;
+}
 
+int main() {
+	pair<int, int> p[9];
+	for (int i = 1; i <= 8; i++) {
+		cin >> p[i].first;
+		p[i].second = i;
+	}
+	sort(p + 1, p + 9);
+
+	int total(0);
+	for (int i = 4; i <= 8; i++) {
+		total += p[i].first;
+	}
+	cout << total << endl;
+
+	sort(p + 4, p + 9, compare);
+	for (int i = 4; i <= 8; i++) {
+		cout <<  p[i].second << " ";
+	}
 }
