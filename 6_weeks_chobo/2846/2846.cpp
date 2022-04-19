74eafc68aa0a3d4594443389b8047a6fa1f8cc61
choihj105 <li0_0li2@naver.com>
2022-04-19 11:31:39 +0900

    feat : Complete '2846'

--- 6_weeks_chobo/2846/2846.cpp
+++ 6_weeks_chobo/2846/2846.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+	int n, MAX =0 , begin =0 , end = 0;
 	
+	
+	cin >> n >> begin;
+	int diff = 0; n--;
+	while (n--) {
+		int tmp; cin >> tmp;
+		if (tmp > begin) {
+			diff += (tmp - begin);
+		}
+		else {
+			MAX = MAX < diff ? diff : MAX;
+			diff = 0;
+		}
+		
+		begin = tmp;
+	}
+
+	MAX = MAX < diff ? diff : MAX;
+	cout << MAX;
 }
