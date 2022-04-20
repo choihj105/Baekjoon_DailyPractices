3662873e902505f094483ce7973b804675a18b24
choihj105 <li0_0li2@naver.com>
2022-04-20 11:04:43 +0900

    refactor : using bitmask

--- 6_weeks_chobo/2789/2789.cpp
+++ 6_weeks_chobo/2789/2789.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-	char tmp;
-	char regulation[9] = {'C', 'A', 'M', 'B', 'R', 'I', 'D', 'G', 'E'};
-	while (cin >> tmp) {
-		for (int i = 0; i < 9; i++) {
-			if (tmp == regulation[i]) {
-				tmp = 0;
-				break;
-			};
-		}
-		if (tmp != 0) cout << tmp;
-	}
+	string str;
+	int t = 135519;
+	cin >> str;
+	for (char c : str) if (~t & (1 << c - 65)) cout << c;
 }
+
+//int main() {
+//	char tmp;
+//	char regulation[9] = {'C', 'A', 'M', 'B', 'R', 'I', 'D', 'G', 'E'};
+//	while (cin >> tmp) {
+//		for (int i = 0; i < 9; i++) {
+//			if (tmp == regulation[i]) {
+//				tmp = 0;
+//				break;
+//			};
+//		}
+//		if (tmp != 0) cout << tmp;
+//	}
+//}
