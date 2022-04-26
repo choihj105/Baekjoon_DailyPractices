14437119613cd547937a7f5fb5add37110bf024f
choihj105 <li0_0li2@naver.com>
2022-04-26 17:39:56 +0900

    feat : Complete '2953'

--- 7_weeks_chobo/2953/2953.cpp
+++ 7_weeks_chobo/2953/2953.cpp
@@ -3,4 +3,18 @@ using namespace std;
 
 int main() {
 
+	int Max[5] = { 0, };
+	for (int i = 0; i < 20; i++) {
+		int tmp; cin >> tmp;
+		Max[i / 4] += tmp;	
+	}
+	
+	int MaxScore = 0;
+	for (int i = 0; i < 5; i++) {
+		MaxScore = Max[i] > MaxScore ? Max[i] : MaxScore;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		if (Max[i] == MaxScore) cout << i+1 << ' ' << MaxScore;
+	}
 }
