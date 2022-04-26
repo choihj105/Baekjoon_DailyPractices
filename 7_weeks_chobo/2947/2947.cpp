31ebfa3d424b1f7152f8d20902d210bdb2440c9c
choihj105 <li0_0li2@naver.com>
2022-04-26 17:15:44 +0900

    feat : Complete '2947'

--- 7_weeks_chobo/2947/2947.cpp
+++ 7_weeks_chobo/2947/2947.cpp
@@ -3,4 +3,30 @@ using namespace std;
 
 int main() {
 
+	int wood[5];
+	for (int i = 0; i < 5; i++) cin >> wood[i];
+
+	while (true) {
+		if (wood[0] > wood[1]) {
+			swap(wood[0], wood[1]);
+			for (auto i : wood) cout << i << ' ';
+			cout << endl;
+		}
+		if (wood[1] > wood[2]) {
+			swap(wood[1], wood[2]);
+			for (auto i : wood) cout << i << ' ';
+			cout << endl;
+		}
+		if (wood[2] > wood[3]) {
+			swap(wood[2], wood[3]);
+			for (auto i : wood) cout << i << ' ';
+			cout << endl;
+		}
+		if (wood[3] > wood[4]) {
+			swap(wood[3], wood[4]);
+			for (auto i : wood) cout << i << ' ';
+			cout << endl;
+		}
+		if (wood[0] == 1 && wood[1] == 2 && wood[2] == 3 && wood[3] == 4 && wood[4] == 5) break;
+	}
 }
