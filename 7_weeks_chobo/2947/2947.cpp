3b9234d125ef458bd70ace2540d289d7deda5ba3
choihj105 <li0_0li2@naver.com>
2022-04-26 17:19:24 +0900

    refactor : '2947' using for loop

--- 7_weeks_chobo/2947/2947.cpp
+++ 7_weeks_chobo/2947/2947.cpp
@@ -7,25 +7,13 @@ int main() {
 	for (int i = 0; i < 5; i++) cin >> wood[i];
 
 	while (true) {
-		if (wood[0] > wood[1]) {
-			swap(wood[0], wood[1]);
-			for (auto i : wood) cout << i << ' ';
-			cout << endl;
-		}
-		if (wood[1] > wood[2]) {
-			swap(wood[1], wood[2]);
-			for (auto i : wood) cout << i << ' ';
-			cout << endl;
-		}
-		if (wood[2] > wood[3]) {
-			swap(wood[2], wood[3]);
-			for (auto i : wood) cout << i << ' ';
-			cout << endl;
-		}
-		if (wood[3] > wood[4]) {
-			swap(wood[3], wood[4]);
-			for (auto i : wood) cout << i << ' ';
-			cout << endl;
+
+		for (int j = 0; j < 4; j++) {
+			if (wood[j] > wood[j + 1]) {
+				swap(wood[j], wood[j+1]);
+				for (auto i : wood) cout << i << ' ';
+				cout << endl;
+			}
 		}
 		if (wood[0] == 1 && wood[1] == 2 && wood[2] == 3 && wood[3] == 4 && wood[4] == 5) break;
 	}
