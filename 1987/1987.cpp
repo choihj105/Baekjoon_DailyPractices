873941111e1fa6789fce7b1d7f4e9f6dbc486e26
choihj105 <li0_0li2@naver.com>
2022-05-01 21:12:14 +0900

    feat : init frame '1987'

---
+++ 1987/1987.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+char board[20][20];
+bool isVisited[20][20];
+bool alpabet[20]; // A ~ Z
+int r, c;
+
+int BFS() {
+	queue<pair<int, int>> q;
+
+}
+
+
+int main() {
+
+
+	// Input
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	int maxDist(0);
+	// Solve
+
+
+
+	// Output
+
+}
