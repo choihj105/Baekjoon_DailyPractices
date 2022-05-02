97df9d0510979a05da17248dc6e8ddd6e98290e5
choihj105 <li0_0li2@naver.com>
2022-05-02 15:23:52 +0900

    Complete '3040' using sort, TC O(nlgn)

--- 8_weeks_chobo/3040/3040.cpp
+++ 8_weeks_chobo/3040/3040.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+// sortingµ» ≥≠¿Ô¿ÃµÈ O(N) + sort O(nlgn) = O(nlgn)
 int main() {
+	int sum(0); int dwarf[9];
 
-}
+	for (int i = 0; i < 9; i++) {
+		cin >> dwarf[i];
+		sum += dwarf[i];
+	}
+	sum -= 100;
+
+	sort(dwarf, dwarf+9);
+
+	int lPivot = 0; int rPivot = 8;
+	while (rPivot > lPivot) {
+		int tmp = dwarf[lPivot] + dwarf[rPivot];
+		
+		if (tmp == sum) break;
+		else if (tmp > sum) rPivot--;
+		else lPivot++;
+	}
+
+	for (int i = 0; i < 9; i++) {
+		if (i == lPivot || i == rPivot) continue;
+		cout << dwarf[i] << '\n';
+	}
+	
+} 
