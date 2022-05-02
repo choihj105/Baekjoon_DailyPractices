64dc3c85167d905e49d6700bddaf49d0e166309d
choihj105 <li0_0li2@naver.com>
2022-05-02 11:26:30 +0900

    feet : Complete '1987' using DFS, char ASCII

--- 1987/1987.cpp
+++ 1987/1987.cpp
@@ -1,34 +1,53 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 
 char board[20][20];
-bool isVisited[20][20];
-bool alpabet[20]; // A ~ Z
+int dy[4] = {-1, 1, 0, 0};
+int dx[4] = {0, 0, -1, 1};
+bool alpabet[26]; // A ~ Z1987
+
 int r, c;
+int maxDist(0);
 
-int BFS() {
-	queue<pair<int, int>> q;
+void switchV(char ch) {
+	alpabet[ch - 'A'] = !alpabet[ch - 'A'];
+}
 
+void DFS(int y, int x, int cnt) {
+
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+		
+		// 한 번 지나간 알파벳인지 확인
+		if (alpabet[board[ny][nx] - 'A']) continue;
+
+		switchV(board[ny][nx]);
+		DFS(ny, nx, cnt + 1);
+		switchV(board[ny][nx]);
+	}
+
+	maxDist = maxDist < cnt ? cnt : maxDist;
 }
 
 
 int main() {
 
-
 	// Input
 	cin >> r >> c;
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			cin >> board[i][j];
+			char tmp; cin >> tmp;
+			board[i][j] = tmp;
 		}
 	}
 
-	int maxDist(0);
 	// Solve
-
-
+	switchV(board[0][0]);
+	DFS(0,0,1);
 
 	// Output
-
+	cout << maxDist;
 }
