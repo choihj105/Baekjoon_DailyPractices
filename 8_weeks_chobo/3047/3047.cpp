31bf92bab4f87bc36585beda1b49398d011a205c
choihj105 <li0_0li2@naver.com>
2022-05-02 15:32:28 +0900

    Complete '3047' using sort

--- 8_weeks_chobo/3047/3047.cpp
+++ 8_weeks_chobo/3047/3047.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
-
+	int num[3];
+	cin >> num[0] >> num[1] >> num[2];
+	sort(num, num + 3); // O(nlgn)
+	
+	for (int i = 0; i < 3; i++) {
+		char tmp; cin >> tmp;
+		cout << num[tmp - 'A'] << ' ';
+	}
 }
