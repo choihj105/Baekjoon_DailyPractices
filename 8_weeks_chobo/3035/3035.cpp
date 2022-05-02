abfe00ed7eb4807c3641159dfa6b5891cba14fb6
choihj105 <li0_0li2@naver.com>
2022-05-02 14:50:29 +0900

    Complete '3035'

--- 8_weeks_chobo/3035/3035.cpp
+++ 8_weeks_chobo/3035/3035.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	int r, c, zr, zc;
+	cin >> r >> c >> zr >> zc;
 
+
+	for (int k = 0; k < r; k++) {
+		string str;
+		for (int i = 0; i < c; i++) {
+			char ch; cin >> ch;
+			for (int j = 0; j < zc; j++) {
+				str += ch;
+			}
+		}
+		for (int i = 0; i < zr; i++) {
+			cout << str << endl;
+		}
+	}
 }
