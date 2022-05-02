dd9440bad31701273692ab6abc9bfa4202256b8f
choihj105 <li0_0li2@naver.com>
2022-05-02 15:39:24 +0900

    Complete '4447'

--- 8_weeks_chobo/4447/4447.cpp
+++ 8_weeks_chobo/4447/4447.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	int n; cin >> n;
+	cin.ignore();
+	string str;
+	while (n--) {
+		int g(0), b(0);
+		getline(cin, str);
 
+		for (auto i : str) {
+			if (i == 'g' || i == 'G') g++;
+			else if (i == 'b' || i == 'B') b++;
+		}
+		cout << str << " is ";
+		if(g == b) cout << "NEUTRAL" << '\n';
+		else if(g > b) cout << "GOOD" << '\n';
+		else cout << "A BADDY" << '\n';
+	}
 }
