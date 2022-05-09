817a0ec6b7cebdfbeb817a481fa91013a0c15fa6
choihj105 <li0_0li2@naver.com>
2022-05-09 20:17:20 +0900

    Complete '5032'

--- 9_weeks_chobo/5032/5032.cpp
+++ 9_weeks_chobo/5032/5032.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+	int e, f, c; // 가진 빈 병, 발견한 빈 병, 새 병으로 바꾸는데 필요한 갯수
+	cin >> e >> f >> c;
 
+	int cnt(0); // 마신 갯수
+	int bottle = e + f;
+	
+	while (bottle >= c) {
+		int newSoda = bottle / c;
+		cnt += newSoda;
+		bottle %= c;
+		bottle += newSoda;
+	}
+	cout << cnt;
+	
 }
