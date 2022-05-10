39ac2d369e8de1706ea456733cd2b35a71372e96
choihj105 <li0_0li2@naver.com>
2022-05-10 11:02:23 +0900

    feat : Complete '5597'

--- 9_weeks_chobo/5597/5597.cpp
+++ 9_weeks_chobo/5597/5597.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+	bool student[31] = {false,};
 
+	for (int i = 0; i < 28; i++) {
+		int tmp; cin >> tmp;
+		student[tmp] = true;
+	}
+
+	for (int i = 1; i < 31; i++) {
+		if (!student[i]) cout << i << endl;
+	}
 }
