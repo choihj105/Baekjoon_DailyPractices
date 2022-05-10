e6e46f08ea09618d5af24b526894c910fa61f458
choihj105 <li0_0li2@naver.com>
2022-05-10 10:49:36 +0900

    feat : Complete '5054'

--- 9_weeks_chobo/5054/5054.cpp
+++ 9_weeks_chobo/5054/5054.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-
+	int t, n;
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		int min(99), max(0);
+		while (n--) {
+			int tmp; cin >> tmp;
+			min = tmp < min ? tmp : min;
+			max = tmp > max ? tmp : max;
+		}
+		cout << 2 * (max - min) << endl;
+	}
 }
