5d46a3bbc7cf1b7fec79462a73ecd931a08469e0
choihj105 <li0_0li2@naver.com>
2022-05-10 10:53:56 +0900

    feat : Complete '5063'

--- 9_weeks_chobo/5063/5063.cpp
+++ 9_weeks_chobo/5063/5063.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+	int n; cin >> n;
+	while (n--) {
+		int r, e, c;
+		cin >> r >> e >> c;
+
+		if ((e - c) > r) cout << "advertise" << endl;
+		else if ((e - c) < r) cout << "do not advertise" << endl;
+		else cout << "does not matter" << endl;
+	}
 
 }
