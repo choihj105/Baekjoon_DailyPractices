88e8750c12b1c38a15912d474fde33e6f0109b65
choihj105 <li0_0li2@naver.com>
2022-05-18 08:50:53 +0900

    feat : Complete '6359'

--- 10_weeks_chobo/6359/6359.cpp
+++ 10_weeks_chobo/6359/6359.cpp
@@ -5,6 +5,17 @@ int main() {
 	int t; cin >> t;
 	while (t--) {
 		int n; cin >> n;
+		bool room[101] = { false, };
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; i*j <= n; j++) {
+				room[i*j] = !room[i * j];
+			}
+		}
 
+		int cnt(0);
+		for (int i = 1; i <= n; i++) {
+			if (room[i]) cnt++;
+		}
+		cout << cnt << endl;
 	}
 }
