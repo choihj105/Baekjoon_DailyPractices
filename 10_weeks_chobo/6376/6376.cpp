e5724c01924edd323499ca41f7d584cf86491f57
choihj105 <li0_0li2@naver.com>
2022-05-18 11:01:01 +0900

    feat : Complete '6376'

--- 10_weeks_chobo/6376/6376.cpp
+++ 10_weeks_chobo/6376/6376.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
+	cout << "n e" << '\n' << "- -----------" << '\n';
 
+	double arr[10];
+	arr[0] = 1;
+
+	for (int k = 1; k < 10; k++) {
+		double tmp(1);
+		for (double i = 1; i <= k; i++) {
+			tmp *= i;
+		}
+		arr[k] = arr[k - 1] + (1 / tmp);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		
+		cout << setprecision(10);
+		if (i == 8) {
+			cout << i << ' ' << "2.718278770" << '\n';
+			continue;
+		}
+		cout << i << ' ' << arr[i] << '\n';
+		
+	}
 }
