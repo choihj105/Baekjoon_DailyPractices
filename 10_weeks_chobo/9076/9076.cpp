d03d299d73b8a87ccb69b61db1aa1a424e7a8007
choihj105 <li0_0li2@naver.com>
2022-05-18 11:29:07 +0900

    feat : Complete '9076'

--- 10_weeks_chobo/9076/9076.cpp
+++ 10_weeks_chobo/9076/9076.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
+	int t; cin >> t;
+	while (t--) {
+		int nums[5];
+		for (int i = 0; i < 5; i++) {
+			cin >> nums[i];
+		}
+		sort(nums, nums+ 5);
 
+		if (nums[3] - nums[1] >= 4) cout << "KIN" << '\n';
+		else cout << nums[1] + nums[2] + nums[3] << '\n';
+	}
 }
