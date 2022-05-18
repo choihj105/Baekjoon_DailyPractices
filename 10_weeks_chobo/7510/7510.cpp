c80cda56df61b7c14e9677703d236be673771218
choihj105 <li0_0li2@naver.com>
2022-05-18 11:14:36 +0900

    feat : Complete '7510'

--- 10_weeks_chobo/7510/7510.cpp
+++ 10_weeks_chobo/7510/7510.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+	int t; cin >> t;
 
+	for (int i = 1; i <= t; i++) {
+		int a,b,c; cin >> a>> b>> c;
+		a *= a;
+		b *= b;
+		c *= c;
+
+		cout << "Scenario #" << i << ':' << '\n';
+		cout << ( (a+b==c || b+c==a || c+a == b ) ? "yes" : "no") << '\n' <<'\n';
+	}
 }
