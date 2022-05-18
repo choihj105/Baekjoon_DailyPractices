2158013e0f6eaccde81a7309bc74f6fc1cc90178
choihj105 <li0_0li2@naver.com>
2022-05-18 11:35:36 +0900

    feat : Complete '9094'

--- 10_weeks_chobo/9094/9094.cpp
+++ 10_weeks_chobo/9094/9094.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n, m; cin >> n >> m;
 
+		int cnt(0);
+		for (int i = 1; i < n-1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if ((i * i + j * j + m) % (i * j) == 0) cnt++;
+			}
+		}
+		cout << cnt << '\n';
+	}
 }
