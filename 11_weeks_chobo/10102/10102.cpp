a1491168e3e456828dd8535b59ed34f4997655be
choihj105 <li0_0li2@naver.com>
2022-05-23 11:13:09 +0900

    feat : Complete '10102'

--- 11_weeks_chobo/10102/10102.cpp
+++ 11_weeks_chobo/10102/10102.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-
+	int v, t = 0;
+	cin >> v;
+	while (v--) {
+		char c;
+		cin >> c;
+		if (c == 'A') t++;
+		else t--;
+	}
+	
+	if (t > 0) cout << 'A';
+	else if (t == 0) cout << "Tie";
+	else cout << 'B';
 }
