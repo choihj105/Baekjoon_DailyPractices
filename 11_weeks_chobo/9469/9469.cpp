b8edf42908f2fa33cf52f02227d092672a509150
choihj105 <li0_0li2@naver.com>
2022-05-23 10:56:22 +0900

    feat : Complete '9469'

--- 11_weeks_chobo/9469/9469.cpp
+++ 11_weeks_chobo/9469/9469.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+	int p; cin >> p;
 
+	while (p--) {
+		int n; 
+		float d, a, b, f;
+		cin >> n >> d >> a >> b >> f;
+		float time = d / (a + b);
+		
+		cout << fixed;
+		cout.precision(6);
+		cout << n << ' ' << f * time << '\n';
+	}
 }
