5319d4065acabc5c6db952a033cda4874aeb98ab
choihj105 <li0_0li2@naver.com>
2022-05-29 16:13:08 +0900

    feat : Complete '10811'

--- 12_weeks_chobo/10811/10811.cpp
+++ 12_weeks_chobo/10811/10811.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
-
+	int n, m; cin >> n >> m;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = i + 1;
+	}
+	while (m--) {
+		int i, j; cin >> i >> j;
+		i--, j--;
+		while (i < j) swap(arr[i++], arr[j--]);
+	}
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << ' ';
+	}
 }
