6c37a93dd6634036f52ec7f144fbd342f7368d45
choihj105 <li0_0li2@naver.com>
2022-06-04 16:03:06 +0900

    feat : Complete 10869, 10871

--- 12_weeks_chobo/10869/10869.cpp
+++ 12_weeks_chobo/10869/10869.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
-
+	int a, b; cin >> a >> b;
+	cout << a + b << '\n'
+		<< a - b << '\n'
+		<< a * b << '\n'
+		<< a / b << '\n'
+		<< a % b;
 }
--- 12_weeks_chobo/10871/10871.cpp
+++ 12_weeks_chobo/10871/10871.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main() {
-
+	int n, x; cin >> n >> x;
+	for (int i = 0; i < n; i++) {
+		int tmp; cin >> tmp;
+		if (tmp < x) cout << tmp << ' ';
+	}
 }
