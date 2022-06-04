d82ec4ac35e7f8a3d0f6c733be7fc7198f71bf7c
choihj105 <li0_0li2@naver.com>
2022-06-04 16:14:16 +0900

    feat : Complete 10987

--- 12_weeks_chobo/10987/10987.cpp
+++ 12_weeks_chobo/10987/10987.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+	char c[5] = { 'a', 'e','i', 'o', 'u' };
+	char t; int cnt(0);
+	while (cin >> t) {
+		for (int i = 0; i < 5; i++) {
+			if (t == c[i]) cnt++;
+		}
+	}
 
+	cout << cnt;
 }
