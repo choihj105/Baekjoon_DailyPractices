6d95f13644ab4c53cc367a091e3402d8d2f7ed4c
choihj105 <li0_0li2@naver.com>
2022-06-08 08:25:00 +0900

    feat : Complete '14581'

--- 13_weeks_chobo/14581/14581.cpp
+++ 13_weeks_chobo/14581/14581.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-
+	string str; cin >> str;
+	
+	for (int i = 0; i < 9; i++) {
+		if (i != 4) cout << ":fan:";
+		else cout << ":" << str << ":";
+		if ((i + 1) % 3 == 0) cout << '\n';
+	}
+	
 }
