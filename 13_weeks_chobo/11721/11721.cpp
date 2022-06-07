73853889a7732c0429dec3ded229b65c6135a7cb
choihj105 <li0_0li2@naver.com>
2022-06-08 08:21:35 +0900

    feat : Complete '14581'

--- 13_weeks_chobo/11721/11721.cpp
+++ 13_weeks_chobo/11721/11721.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-	int cnt(0);
-	while (++cnt) {
-		char c; cin >> c;
+	int cnt(0); char c;
+	while (cin >> c) {
+		cnt++;
 		cout << c;
 		if (cnt%10 == 0) cout << '\n';
 	}
