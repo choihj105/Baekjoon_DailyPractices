76ee59cc0896042d57e9c93470d1ef6b4e598c42
choihj105 <li0_0li2@naver.com>
2022-06-08 09:07:12 +0900

    feat : Complete '14582'

--- 13_weeks_chobo/14582/14582.cpp
+++ 13_weeks_chobo/14582/14582.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+	int a[10], b[10];
+	a[0] = 0; b[0] = 0;
+	for (int i = 1; i < 10; i++) {
+		int tmp; cin >> tmp;
+		a[i] = (a[i - 1] + tmp);
+	}
+	
+	for (int i = 1; i < 10; i++) {
+		int tmp; cin >> tmp;
+		b[i] = (b[i - 1] + tmp);
+	}
 
+	for (int i = 1; i < 10; i++) {
+		if (a[i] > b[i-1]) {
+			cout << "Yes";
+			break;
+		}
+		if (i == 9) cout << "No";
+	}
 }
