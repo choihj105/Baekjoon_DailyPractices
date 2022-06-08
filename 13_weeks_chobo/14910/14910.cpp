dec472778ba09677092ed50c91833d3a83395593
choihj105 <li0_0li2@naver.com>
2022-06-08 09:34:35 +0900

    feat : Complete '14910'

--- 13_weeks_chobo/14910/14910.cpp
+++ 13_weeks_chobo/14910/14910.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
-
+	int prev = -1000001, cur;
+	bool isFlag = true;
+	while (cin >> cur) {
+		if (cin.eof()) break;
+		if (prev > cur) {
+			isFlag = false;
+		}
+		prev = cur;
+	}
+	if (isFlag) cout << "Good";
+	else cout << "Bad";
 }
