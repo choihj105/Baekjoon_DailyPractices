87eaffada4e55ce49ddc91ea6f092bffd19bcb05
choihj105 <li0_0li2@naver.com>
2022-06-14 13:34:41 +0900

    feat : Complete '15596'

--- 14_weeks_chobo/15596/15596.cpp
+++ 14_weeks_chobo/15596/15596.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
+#include <vector>
+long long sum(std::vector<int>& a) {
+	long long ans = 0;
+	for (int i = 0; i < a.size(); i++) {
+		ans += a[i];
+	}
+	return ans;
 }
