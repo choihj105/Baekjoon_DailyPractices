2e293f1c05750eca0f845588cdfc9a7bc07fa5ea
choihj105 <li0_0li2@naver.com>
2022-06-14 13:50:05 +0900

    feat : Complete '15734'

--- 14_weeks_chobo/15734/15734.cpp
+++ 14_weeks_chobo/15734/15734.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-
+	int l, r, a;
+	cin >> l >> r >> a;
+	
+	int diff = (l - r > 0) ? l - r : r - l;
+	if (a == 0) {
+		if (l > r) cout << r * 2;
+		else cout << l * 2;
+	}
+	else if (a > diff) {
+		if ((a - diff) % 2 == 0) cout << l + r + a;
+		else cout << l + r + a - 1;
+	}
+	else {
+		if (l > r) cout << 2 * (r + a);
+		else cout << 2 * (l + a);
+	}
+	
 }
