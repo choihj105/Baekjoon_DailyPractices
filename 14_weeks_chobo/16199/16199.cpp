7317d7c632af0c46bc009d40ac4e65ed916210af
choihj105 <li0_0li2@naver.com>
2022-06-16 18:03:35 +0900

    feat : Completle '16199'

--- 14_weeks_chobo/16199/16199.cpp
+++ 14_weeks_chobo/16199/16199.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+	int y, m, d, _y, _m, _d;;
+	cin >> y >> m >> d >> _y >> _m >> _d;
 
+	// 만나이
+	if (_y > y) {
+		if (_m > m) {
+			cout << _y - y << '\n';
+		}
+		else if (_m == m) {
+			if (_d >= d) cout << _y - y << '\n';
+			else cout << _y - y - 1 << '\n';
+		}
+		else {
+			cout << _y - y - 1 << "\n";
+		}
+	}
+	else cout << 0 << '\n';
+
+	// 세는 나이
+	cout << _y - y + 1 << '\n';
+
+	// 연 나이
+	cout << _y - y;
 }
