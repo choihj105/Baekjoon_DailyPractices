b2cf7f7304bbd5870716dbeef7f45bb4a4ee74ff
choihj105 <li0_0li2@naver.com>
2022-06-16 18:07:49 +0900

    feat : COmpletle '16435'

--- 14_weeks_chobo/16435/16435.cpp
+++ 14_weeks_chobo/16435/16435.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
+	int n, l; cin >> n >> l;
+	int h[1000];
+	for (int i = 0; i < n; i++) {
+		cin >> h[i];
+	}
 
+	sort(h, h+n);
+	for (int i = 0; i < n; i++) {
+		if (l >= h[i]) l++;
+		else break;
+	}
+
+	cout << l;
 }
