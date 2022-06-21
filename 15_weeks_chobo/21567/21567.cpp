8a527fc9798d25b7d855220cbc9a63b507eae3ab
choihj105 <li0_0li2@naver.com>
2022-06-21 13:46:03 +0900

    feat : Complete '21567'

--- 15_weeks_chobo/21567/21567.cpp
+++ 15_weeks_chobo/21567/21567.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+	long long int a, b, c, t;
+	int check[10] = { 0, };
+	cin >> a >> b >> c;
+	t = a * b * c;
+	while (t) {
+		check[t % 10]++;
+		t /= 10;
+	}
 
+	for (int i = 0; i < 10; i++) {
+		cout << check[i] << '\n';
+	}
 }
