e1e4049ce25b616941db74cbd5d49f07ef9b2017
choihj105 <li0_0li2@naver.com>
2022-06-21 13:41:17 +0900

    feat : Complete '20361'

--- 15_weeks_chobo/20361/20361.cpp
+++ 15_weeks_chobo/20361/20361.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-
+	int n, x, k;
+	cin >> n >> x >> k;
+	while (k--) {
+		int a, b;
+		cin >> a >> b;
+		if (a == x) x = b;
+		else if (b == x) x = a;
+	}
+	cout << x;
 }
