28ff9eb791c029027b96d2d9e25d69c4f0f0e7a2
choihj105 <li0_0li2@naver.com>
2022-06-21 13:51:34 +0900

    feat : Complete '23348'

--- 15_weeks_chobo/23348/23348.cpp
+++ 15_weeks_chobo/23348/23348.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+	int a, b, c, n;
+	cin >> a >> b >> c >> n;
+	
+	int total = -1;
+	while (n--) {
 
+		int t(0);
+		for (int i = 0; i < 3; i++) {
+			int x, y, z;
+			cin >> x >> y >> z;
+			t += (x * a + y * b + z * c);
+		}
+		total = total < t ? t : total;
+	}
+	cout << total;
 }
