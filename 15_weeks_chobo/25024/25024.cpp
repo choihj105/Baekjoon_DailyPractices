b29a293d078e0aae662d1cb3264f080a83dd6f4a
choihj105 <li0_0li2@naver.com>
2022-06-21 17:24:33 +0900

    feat : Complete '25024'

--- 15_weeks_chobo/25024/25024.cpp
+++ 15_weeks_chobo/25024/25024.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+	int t; cin >> t;
+	while (t--) {
+		int x, y; cin >> x >> y;
+		cout << (x < 24 && y < 60 ? "Yes" : "No") << ' ';
+		
+		bool isFlag = false;
+		if (x < 13 && x > 0 && y < 32 && y > 0) {
+			isFlag = true;
+			if (x == 2 && y > 29) isFlag = false;
+			if ((x == 4 || x == 6 || x == 9 || x == 11 ) && y > 30) isFlag = false;
+		}
+		cout << (isFlag ? "Yes" : "No") << '\n';
+	}
 }
