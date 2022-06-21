57a49a6e5e5e41dd03111c5a98f41153bec728f9
choihj105 <li0_0li2@naver.com>
2022-06-21 17:03:50 +0900

    feat : Complete '23972'

--- 15_weeks_chobo/23972/23972.cpp
+++ 15_weeks_chobo/23972/23972.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+	long long k, n;
+	cin >> k >> n;
+	
+	if (n == 1) cout << -1; 	
+	else {
+		cout << ((k * n) % (n - 1) ? k * n / (n - 1) + 1 : k * n / (n - 1));
+	}	
 }
