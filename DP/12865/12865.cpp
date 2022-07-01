658aa59a5d8c3b99d1c5a34368e5ed88ecad260c
choihj105 <li0_0li2@naver.com>
2022-07-01 09:20:20 +0900

    feat : Complete '12865'

--- DP/12865/12865.cpp
+++ DP/12865/12865.cpp
@@ -5,15 +5,20 @@ using namespace std;
 
 int main() {
 	int n, k; cin >> n >> k;
-	vector<pair<int, int>> v(n);
-	vector<int> dp(k + 1);
+	vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+	vector<pair<int, int>> v(n+1);
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cin >> v[i].first >> v[i].second;
 	}
 
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			
+			if (j - v[i].first >= 0) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v[i].first] + v[i].second);
+			else dp[i][j] = dp[i - 1][j];
 
-
-	dp[0] = 0;
-
+		}
+	}
+	cout << dp[n][k];
 }
